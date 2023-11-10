8d1112a0530711d6a50b444297bfba50de8dea3b
Slavik6661 <Slavik0.75@mail.ru>
2023-11-10 22:02:45 +0300

    Update blank.ts
    Add FileOrFolder interface

--- DocuManager/src/interfaces/blank.ts
+++ DocuManager/src/interfaces/blank.ts
@@ -1,5 +1,14 @@
+<<<<<<< Updated upstream
 export interface HeadersObject {
   Accept: string;
   'Content-Type': string;
   Authorization: string;
 }
+=======
+export interface FileOrFolder {
+    name: string;
+    type: 'file' | 'dir';
+    size: number; 
+    created: string; 
+  } 
+>>>>>>> Stashed changes
