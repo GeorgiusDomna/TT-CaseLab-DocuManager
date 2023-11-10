c44a03ba1beca10607686507e5695acd7f0c3e93
Nimboo1 <qwertyuniverse12@yandex.ru>
2023-11-10 13:00:06 +0300

    fix: fix some paths

--- DocuManager/src/components/ContentBlock/ContentBlock.tsx
+++ DocuManager/src/components/ContentBlock/ContentBlock.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import DocumentItem from '../DocumentItem/DocumentItem';
-import classes from './contentBlock.module.css';
+import styles from './contentBlock.module.css';
 import documentData from '../../interfaces/documentData';
 
 interface ContentBlockProps {
@@ -11,17 +11,17 @@ interface ContentBlockProps {
 function ContentBlock({ data, header }: ContentBlockProps) {
   const [documentList, setDocumentList] = useState(data);
 
-  function addItem(data: documentData) {
-    setDocumentList((list) => [...list, data]);
-  }
-  function deleteDocument(id: number) {
-    setDocumentList((list) => list.filter((el) => el.id !== id));
-  }
+  // function addItem(data: documentData) {
+  //   setDocumentList((list) => [...list, data]);
+  // }
+  // function deleteDocument(id: number) {
+  //   setDocumentList((list) => list.filter((el) => el.id !== id));
+  // }
 
   return (
-    <div className={classes.contentBlock}>
-      <h2 className={classes.contentBlock__title}>{header}</h2>
-      <ul className={classes.contentBlock__documentList}>
+    <div className={styles.contentBlock}>
+      <h2 className={styles.contentBlock__title}>{header}</h2>
+      <ul className={styles.contentBlock__documentList}>
         {documentList.map((item) => (
           <DocumentItem key={item.id} data={item} />
         ))}
