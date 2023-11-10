139daece6cc141a696bc84c9fe0e04d540a00b51
Slavik6661 <Slavik0.75@mail.ru>
2023-11-10 22:51:29 +0300

    fetchFolderContents

--- DocuManager/src/interfaces/blank.ts
+++ DocuManager/src/interfaces/blank.ts
@@ -1,14 +1,12 @@
-<<<<<<< Updated upstream
 export interface HeadersObject {
   Accept: string;
   'Content-Type': string;
   Authorization: string;
 }
-=======
 export interface FileOrFolder {
     name: string;
     type: 'file' | 'dir';
     size: number; 
     created: string; 
   } 
->>>>>>> Stashed changes
+
