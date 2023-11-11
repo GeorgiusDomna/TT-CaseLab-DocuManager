0482ad4820ebc67a1cdc6915a764f7b56fe75a38
Slavik6661 <Slavik0.75@mail.ru>
2023-11-11 18:05:51 +0300

    fix

--- DocuManager/src/api/documentService.ts
+++ DocuManager/src/api/documentService.ts
@@ -1,21 +1,24 @@
 import { HeadersObject, FileOrFolder } from '../interfaces/blank';
+import { isOnline } from '../utils/blank';
 
 const OAuth_token: string = import.meta.env.VITE_OAUTH_TOKEN;
 
-const headers: HeadersObject = new Headers({
+const headers: HeadersObject = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
   Authorization: OAuth_token,
-});
+};
 
 export async function fetchFolderContents() {
+  if (!isOnline()) {
+    console.error('Отсутствует подключение к интернету.');
+    return;
+  }
   const url: string = 'https://cloud-api.yandex.net/v1/disk/resources?path=/CaseLabDocuments';
   try {
     let response = await fetch(url, {
       method: 'GET',
-      headers: {
-        Authorization: `OAuth ${OAuth_token}`,
-      },
+      headers: headers,
     });
     if (!response.ok) {
       throw new Error(`Ошибка: ${response.status}`);
