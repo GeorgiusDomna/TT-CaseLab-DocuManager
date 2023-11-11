7ee85c89262621785deed71312185ce82b572e85
Nimboo1 <qwertyuniverse12@yandex.ru>
2023-11-11 22:01:56 +0300

    fix: link for all documents

--- DocuManager/src/components/CategoryItem/CategoryItem.tsx
+++ DocuManager/src/components/CategoryItem/CategoryItem.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function CategoryItem({ data }: { data: string }) {
   return (
     <div className={styles.categoryItem}>
-      <NavLink to={`/categories/${data}`}>{data}</NavLink>
+      <NavLink to={data === 'Все категории' ? '/' : `/categories/${data}`}>{data}</NavLink>
     </div>
   );
 }
