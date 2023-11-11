c001f19e4938b2a39867e80710674d38d758618c
Slavik6661 <Slavik0.75@mail.ru>
2023-11-11 12:25:12 +0300

    documentService

--- DocuManager/src/api/documentService.ts
+++ DocuManager/src/api/documentService.ts
@@ -1,4 +1,4 @@
-import { HeadersObject,FileOrFolder } from '../interfaces/blank';
+import { HeadersObject, FileOrFolder } from '../interfaces/blank';
 
 const OAuth_token: string = import.meta.env.VITE_OAUTH_TOKEN;
 
@@ -8,26 +8,22 @@ const headers: HeadersObject = new Headers({
   Authorization: OAuth_token,
 });
 
-
-export async function  fetchFolderContents(){
-    const url:string="https://cloud-api.yandex.net/v1/disk/resources?path=/CaseLabDocuments"
-    try{
-    let response=await fetch(url,{
-        method:'GET',
-        headers: {
-            Authorization: `OAuth ${OAuth_token}`,
-          },
-         
-    })
-    if(!response.ok){
-       throw new Error(`Ошибка: ${response.status}`)     
+export async function fetchFolderContents() {
+  const url: string = 'https://cloud-api.yandex.net/v1/disk/resources?path=/CaseLabDocuments';
+  try {
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `OAuth ${OAuth_token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.status}`);
     }
-    const data = await response.json() as { _embedded: { items: FileOrFolder[] } };
-    const contents:FileOrFolder[]=data._embedded.items
-    return contents
-    
-}
-catch(err){
+    const data = (await response.json()) as { _embedded: { items: FileOrFolder[] } };
+    const contents: FileOrFolder[] = data._embedded.items;
+    return contents;
+  } catch (err) {
     console.error('Ошибка при получении содержимого папки "CaseLab":', err);
+  }
 }
-} 
