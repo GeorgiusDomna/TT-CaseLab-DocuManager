f21bdda57c8f2d616ffcdfa018d61561cad151bc
Slavik6661 <Slavik0.75@mail.ru>
2023-11-11 12:28:07 +0300

    add CategoryItem

--- DocuManager/src/components/CategoryItem/CategoryItem.tsx
+++ DocuManager/src/components/CategoryItem/CategoryItem.tsx
@@ -1,7 +1,30 @@
 import styles from './categoryItem.module.css';
+import { useEffect, useState } from "react"
+import { fetchFolderContents } from '../../api/documentService';
+import { FileOrFolder } from '../../interfaces/blank';
+import React from 'react';
+export const CategoryItem = () => {
+  const [data, setData] = useState<FileOrFolder[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchFolderContents();
+      setData(result || []);
+     
+      
+    };
+    fetchData();
+    console.log(data);
+  }, [])
+  return (
+    <div className={styles.categoryItem}>
+      {
+        data.map((item, id) =>
+          <div key={id}>{item.name}</div >
+        )
+      }
+    </div>
 
-function CategoryItem({ data }) {
-  return <div className={styles.categoryItem}>{data}</div>;
+  )
 }
 
 export default CategoryItem;
