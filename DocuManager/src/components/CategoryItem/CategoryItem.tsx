5e94cc8afa103925fb9dae75031c2d36e4a2f1db
Slavik6661 <Slavik0.75@mail.ru>
2023-11-11 18:45:18 +0300

    fix

--- DocuManager/src/components/CategoryItem/CategoryItem.tsx
+++ DocuManager/src/components/CategoryItem/CategoryItem.tsx
@@ -3,12 +3,9 @@ import { useEffect, useState } from "react"
 import { fetchFolderContents } from '../../api/documentService';
 import { FileOrFolder } from '../../interfaces/blank';
 
-
-
 export const CategoryItem= () => {
 const handleItemClick=(itemName:string)=>{
-  console.log(`Выбранная категория ${itemName}`);
-  
+  console.log(`Выбранная категория ${itemName}`); 
 }
 
   const [data, setData] = useState<FileOrFolder[]>([]);
@@ -32,8 +29,3 @@ const handleItemClick=(itemName:string)=>{
 }
 
 export default CategoryItem;
-
-{/* interface CategoryItemProps{
-  categories:string[];
-  onItemClick:(category:string)=>void
-} */}
