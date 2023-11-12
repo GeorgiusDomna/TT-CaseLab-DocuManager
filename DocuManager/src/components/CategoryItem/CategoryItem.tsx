d1309f1b08521f0ec52710b283e3cbb5e34db5a9
Slavik6661 <Slavik0.75@mail.ru>
2023-11-12 12:24:50 +0300

    fix

--- DocuManager/src/components/CategoryItem/CategoryItem.tsx
+++ DocuManager/src/components/CategoryItem/CategoryItem.tsx
@@ -1,31 +1,9 @@
 import styles from './categoryItem.module.css';
-import { useEffect, useState } from "react"
-import { fetchFolderContents } from '../../api/documentService';
-import { FileOrFolder } from '../../interfaces/blank';
 
-export const CategoryItem= () => {
-const handleItemClick=(itemName:string)=>{
-  console.log(`Выбранная категория ${itemName}`); 
+interface CategoryItemProps {
+  category: string;
 }
-
-  const [data, setData] = useState<FileOrFolder[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetchFolderContents();
-      setData(result || []); 
-    };
-    fetchData();
-  }, [])
-  return (
-    <>
-      {
-        data.map((item, id) =>(
-          <li key={id} onClick={()=>handleItemClick(item.name)}className={styles.categoryItem}>{item.name}</li>
-        )
-      )}
-    </>
-
-  )
-}
-
+const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
+  return <li className={styles.categoryItem}>{category}</li>;
+};
 export default CategoryItem;
--- DocuManager/src/components/Navigation/Navigation.tsx
+++ DocuManager/src/components/Navigation/Navigation.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 import CategoryItem from '../CategoryItem/CategoryItem';
+import { fetchFolderContents } from '../../api/documentService';
+import { FileOrFolder } from '../../interfaces/blank';
 import styles from './navigation.module.css';
 
 function Navigation() {
-
+  const [data, setData] = useState<FileOrFolder[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchFolderContents();
+      setData(result || []);
+    };
+    fetchData();
+  }, []);
   return (
-   <>
+    <>
       <h3>Категории</h3>
       <ul className={styles.navBar}>
-          <CategoryItem/>
+        {data.map((item, id: number) => (
+          <CategoryItem key={id} category={item.name} />
+        ))}
       </ul>
-   </>
+    </>
   );
 }
 
