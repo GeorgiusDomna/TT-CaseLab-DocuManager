6dfa16bbee66850b22a08558b02ec13c340070e1
EvgeniiyaR <tulpansik@mail.ru>
2023-11-13 23:03:49 +0300

    feat: add edit button and change display logic

--- DocuManager/src/components/DocumentItem/DocumentItem.tsx
+++ DocuManager/src/components/DocumentItem/DocumentItem.tsx
@@ -3,17 +3,56 @@ import styles from './documentItem.module.css';
 interface DocumentItemProps {
   data: string;
 }
+
 const DocumentItem: React.FC<DocumentItemProps> = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenRenamePanel, setIsOpenRenamePanel] = useState(false);
+  const [isOpenMovePanel, setIsOpenMovePanel] = useState(false);
+
   const [selectValue, setSelectValue] = useState('');
+  const [newNameValue, setNewNameValue] = useState('');
 
   const handleSelectValue = (event: ChangeEvent<HTMLSelectElement>) => {
     setSelectValue(event.target.value);
   };
 
+  const handleChangeNewNameValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewNameValue(event.target.value);
+  };
+
   const toggleOption = () => {
     setIsOpen(!isOpen);
+    setIsOpenRenamePanel(false);
+    setIsOpenMovePanel(false);
+  };
+
+  const toggleMovePanel = () => {
+    setIsOpenMovePanel(!isOpenMovePanel);
+    setIsOpenRenamePanel(false);
     setSelectValue('');
+    setNewNameValue('');
+  };
+
+  const toggleRenamePanel = () => {
+    setIsOpenRenamePanel(!isOpenRenamePanel);
+    setIsOpenMovePanel(false);
+    setSelectValue('');
+    setNewNameValue('');
+  };
+
+  const resetForm = () => {
+    setIsOpenRenamePanel(false);
+    setIsOpenMovePanel(false);
+    setSelectValue('');
+    setNewNameValue('');
+  };
+
+  const handleDeleteDocument = () => {
+    resetForm();
+  };
+
+  const handleViewDocument = () => {
+    resetForm();
   };
 
   return (
@@ -23,52 +62,69 @@ const DocumentItem: React.FC<DocumentItemProps> = ({ data }) => {
           <div className={styles.document__iconDocument}></div>
           <p className={styles.document__title}>{data}</p>
         </div>
-        {isOpen ? (
-          <ul className={styles.document__buttons}>
-            <li>
-              <button
-                className={[
-                  styles.document__buttonIcon,
-                  styles.document__buttonIcon_type_view,
-                ].join(' ')}
-                type='button'
-                title='Просмотреть документ'
-              ></button>
-            </li>
-            <li>
-              <button
-                className={[
-                  styles.document__buttonIcon,
-                  styles.document__buttonIcon_type_toggle,
-                ].join(' ')}
-                type='button'
-                title='Скрыть панель'
-                onClick={toggleOption}
-              ></button>
-            </li>
-            <li>
-              <button
-                className={[
-                  styles.document__buttonIcon,
-                  styles.document__buttonIcon_type_busket,
-                ].join(' ')}
-                type='button'
-                title='Удалить документ'
-              ></button>
-            </li>
-          </ul>
-        ) : (
-          <button
-            className={[styles.document__buttonIcon, styles.document__buttonIcon_type_option].join(
-              ' '
-            )}
-            type='button'
-            title='Открыть панель'
-            onClick={toggleOption}
-          ></button>
-        )}
+        <ul className={styles.document__buttons}>
+          {isOpen && (
+            <>
+              <li>
+                <button
+                  className={[
+                    styles.document__buttonIcon,
+                    styles.document__buttonIcon_type_view,
+                  ].join(' ')}
+                  type='button'
+                  title='Просмотреть документ'
+                  onClick={handleViewDocument}
+                ></button>
+              </li>
+              <li>
+                <button
+                  className={[
+                    styles.document__buttonIcon,
+                    styles.document__buttonIcon_type_rename,
+                  ].join(' ')}
+                  type='button'
+                  title='Переименовать документ'
+                  onClick={toggleRenamePanel}
+                ></button>
+              </li>
+              <li>
+                <button
+                  className={[
+                    styles.document__buttonIcon,
+                    styles.document__buttonIcon_type_toggle,
+                  ].join(' ')}
+                  type='button'
+                  title='Переместить документ'
+                  onClick={toggleMovePanel}
+                ></button>
+              </li>
+              <li>
+                <button
+                  className={[
+                    styles.document__buttonIcon,
+                    styles.document__buttonIcon_type_busket,
+                  ].join(' ')}
+                  type='button'
+                  title='Удалить документ'
+                  onClick={handleDeleteDocument}
+                ></button>
+              </li>
+            </>
+          )}
+          <li>
+            <button
+              className={[
+                styles.document__buttonIcon,
+                styles.document__buttonIcon_type_option,
+              ].join(' ')}
+              type='button'
+              title='Открыть панель взаимодействия с документом'
+              onClick={toggleOption}
+            ></button>
+          </li>
+        </ul>
       </div>
-      {isOpen && (
+      {isOpenMovePanel && (
         <form className={styles.document__form} name='change-document'>
           <select
             className={styles.document__select}
@@ -83,6 +139,20 @@ const DocumentItem: React.FC<DocumentItemProps> = ({ data }) => {
           </select>
           <button className={styles.document__button} type='submit' disabled={!selectValue}>
             Переместить
+          </button>
+        </form>
+      )}
+      {isOpenRenamePanel && (
+        <form className={styles.document__form} name='change-document'>
+          <input
+            className={styles.document__select}
+            type='text'
+            placeholder='Новое название'
+            value={newNameValue}
+            onChange={handleChangeNewNameValue}
+          />
+          <button className={styles.document__button} type='submit' disabled={!newNameValue}>
+            Переименовать
           </button>
         </form>
       )}
