58979d979ab465cccb68032d47289463db61d643
Slavik6661 <Slavik0.75@mail.ru>
2023-11-13 21:10:54 +0300

    new

--- DocuManager/src/components/CategoryItem/CategoryItem.tsx
+++ DocuManager/src/components/CategoryItem/CategoryItem.tsx
@@ -1,6 +1,6 @@
 import styles from './categoryItem.module.css';
 import { NavLink } from 'react-router-dom';
-
+//import icons from './icons/folder.svg'
 interface CategoryItemProps {
   category: string;
 }
@@ -9,6 +9,7 @@ const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
   const encodeData = encodeURIComponent(category);
   return (
     <li className={styles.categoryItem}>
+      <img src={`./icons/folder.svg`} alt={category} />
       <NavLink to={`/categories/${encodeData}`}>{category}</NavLink>
     </li>
   );
